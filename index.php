78ba0d783b216bfb1ce74d01b490abaa41a3d4ea
jeremynixon <jnixon2@gmail.com>
2014-06-27 10:43:03 -0400

    added P1 content

--- index.php
+++ index.php
@@ -1,7 +1,65 @@
-<h1>Hello World</h1>
 
-<h2>Jeremy's Web Page</h2>
+<head> 
+	<title>Jeremy Nixon</title>
+	<link rel=stylesheet type="text/css" href="style.css">
+</head>
 
-<img src="jeremy.jpg">
+<body id="body">
 
-<h6>Scratch Beginnings...</h6>
+<div>
+	<div id="hello">
+		<h1><b>Hello World!</b></h1>
+	</div>
+
+	<div id="title">
+		<h2>Jeremy's Web Page</h2>
+	</div>
+
+
+	<div class="text" id="whatido">
+		<p>I'm a Junior at Harvard College, concentrating in <em>Applied Mathematics and Economics</em> with a strong focus on Behavioral Economics.
+		A partner and I built an Iphone app called Timestamp last summer, a quantified self app dedicated to time tracking.
+		I've been really interested in productivity and decision making my whole life.</p>
+	</div>
+
+	<div >
+		<img id="jeremy" src="jeremydisc.jpg">
+	</div>
+
+	<div class="text" id="course">
+		<p>I'm extremely motivated to be able to create. The inability to create effective websites and web applications
+		 gave me difficulty in promoting a product and interacting with potential users. I want to have those skills on hand.
+		 I also want to turn creative ideas for websites into a real online presence. 
+		</p>
+	</div>
+
+	<div class="text" id="webexp">
+		<p>I've spent a lot of time with online tutorials and textbooks on HTML and CSS, and I used Ruby on Rails to create a rudimentary
+		 interactive website last summer. I'm very familiar with version control using git.</p>
+	</div>
+
+	<div class="text" id="programexp">
+		<p></p>
+	</div>
+
+	<div class="text" id="php">
+		<p>I followed the first 4 chapters of a text in PHP in preperation for this course, covering: <br>
+			<ul>
+				<li>Variables and Operators
+				<li>Conditional Statements
+				<li>Loops
+				<li>Arrays			
+			</ul>
+		Which is the extent of my PHP experience.
+		</p>
+	</div>
+
+	<div class="text" id="system">
+		<p>I'm working on a personal Dell Latitude laptop. I'm accustomed to coding in Ubuntu, so using WAMP will be an interesting (and so far convenient!) change.</p>
+
+	<div class="text" id="everythingelse">
+		<p></p>
+	</div>
+</div>
+</body>
+
