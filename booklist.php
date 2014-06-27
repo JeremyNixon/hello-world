1d4a2e98f977f24b8584bb33bfd7e5e81744d43e
jeremynixon <jnixon2@gmail.com>
2014-06-27 11:20:29 -0400

    added headings

---
+++ booklist.php
@@ -0,0 +1,280 @@
+<p>Master Book List<br />
+ <br />
+Table of Contents<br />
+List Description and Purpose<br />
+Priority and Choice<br />
+Philosophy<br />
+Ethics<br />
+Metaphysics<br />
+Logic<br />
+Epistemology<br />
+Linguistics<br />
+Religion<br />
+Economics<br />
+Behavioral Economics<br />
+Finance<br />
+Political Science/Government/Foreign Policy<br />
+History<br />
+Psychology/Mind, Brain, Behavior<br />
+Sociology<br />
+Statistics<br />
+Mathematics<br />
+Sciences<br />
+Physics<br />
+Biology<br />
+Chemistry<br />
+Environment<br />
+Computer Science<br />
+Literature<br />
+Poetry<br />
+Plays<br />
+Comedy<br />
+Non-Fiction<br />
+Fiction<br />
+Science Fiction<br />
+Fantasy<br />
+Personal Growth<br />
+Athletics<br />
+Personal Skills<br />
+Relationships<br />
+Business<br />
+Startups <br />
+ </p>
+
+<p> </p>
+
+<p>1. List Description and Purpose</p>
+
+<p>I want a comprehensive categorization of my idea-space. I would like it to generalize to human idea-space, but I doubt that a &lsquo;complete&rsquo; list &ndash; categorizing all relevant books in a field, and having every category of book &ndash; would be practical or efficient.<br />
+This list should make my book choices more aware of opportunity costs and allow me to consider a wider set of options. I should also be able to see holes in my knowledge space.<br />
+ <br />
+2. Priority and Choice</p>
+
+<p>Whenever possible, I&rsquo;ll read books that will be readily applicable to the next month of my life. I&rsquo;ll pay attention to them while reading because I&rsquo;m anticipating their application, and then in applying them I will encode more of the material into memory by actively applying it.</p>
+
+<p>3. Philosophy</p>
+
+<p>Ethics<br />
+Spinoza. The Ethics.<br />
+Seneca. Letters From a Stoic.<br />
+Aurelius. Meditations.</p>
+
+<p>Metaphysics<br />
+Kant. Critique of Pure Reason.<br />
+Schopenhauer. The World as Will and Representation.</p>
+
+<p>Logic<br />
+Goldfarb. Deductive Logic.</p>
+
+<p>Epistemology<br />
+Hume. A Treatise of Human Nature.<br />
+Yudkowski. Less Wrong series on Epistemology.</p>
+
+<p>Linguistics<br />
+Pinker. Words and Rules.<br />
+Pinker. Stuff of Thought. Language as a Window into Human Nature.</p>
+
+<p>Religion<br />
+ </p>
+
+<p>4. Economics</p>
+
+<p>Piketty. Capital.<br />
+Robinson. Why Nations Fail.<br />
+Mullainathan. Scarcity.<br />
+Banerjee. Poor Economics.</p>
+
+<p>Behavioral Economics<br />
+Ariley. Predictably Irrational.<br />
+Thaler, Sunstein. Nudge.<br />
+Khaneman. Thinking Fast and Slow.</p>
+
+<p>
+Finance<br />
+Michael Lewis. The Big Short. Looking at the credit and housing crisis, and those who bet against the market.</p>
+
+<p>5. Political Science/Government/Foreign Policy</p>
+
+<p>Perkins. Confessions of an Economic Hitman.<br />
+William Blum. Killing Hope. US and CIA anti-intervention.<br />
+Foreign Affairs Magazine<br />
+Marx. The Communist Manifesto.<br />
+Hobbes. Leviathan.<br />
+Machiavelli. The Prince.</p>
+
+<p>6. History</p>
+
+<p>Nietzsche. The Use and Abuse of History.<br />
+Loewen. Lies my Teacher Told Me.<br />
+Zinn. A People&rsquo;s History of the United States.<br />
+Diamond. Guns, Germs and Steel.</p>
+
+<p>7. Psychology/Mind, Body, Behavior</p>
+
+<p>Hofstadter. Godel, Escher, Bach<br />
+Klein. Sources of Power.<br />
+Susan Cain. Quiet. The power of introverts.<br />
+Edward de Bono. Six Thinking Hats. Parallel thinking and structuring thinking.<br />
+Watts. Everything is Obvious. Once you know the answer, how common sense fails us.<br />
+Simon. In Sheep&rsquo;s Clothing. Manipulative People.<br />
+Ariley. The Honest Truth about Dishonesty.<br />
+McGonigal. Willpower.<br />
+Gladwell. Blink. The Power of Thinking Without Thinking. <br />
+Dennett. Intuition Pumps and Other Tools for Thinking.<br />
+ <br />
+8. Sociology</p>
+
+<p>Nesbitt. Intelligence and How to Get It. Why schools and cultures count.<br />
+Griffin. Black Like Me.<br />
+Levitt. Think Like a Freak.<br />
+Gladwell. The Tipping Point.<br />
+Foucault. Power/Knowledge. <br />
+Foucault. Discipline and Punish. </p>
+
+<p>9. Statistics</p>
+
+<p>Taleb. The Black Swan<br />
+Taleb. Fooled by Randomness<br />
+Taleb. Antifragile<br />
+Silver. The Signal and the Noise<br />
+McGrayne. The Theory That Would not Die. Bayes rule history.</p>
+
+<p>10. Mathematics</p>
+
+<p>11. Sciences</p>
+
+<p>Bryson. A Short History of Nearly Everything.</p>
+
+<p>Physics<br />
+Feynman. The Feynman Lectures on Physics.</p>
+
+<p>Biology<br />
+Dawkins. The Extended Phenotype.<br />
+Dawkins. The Selfish Gene.</p>
+
+<p>Chemistry</p>
+
+<p>Environment</p>
+
+<p>Pop Science<br />
+Hawking. A Brief History of Time.<br />
+Hawking. The Grand Design. Examines scientific knowledge about the universe as an alternative to intelligent design.<br />
+ <br />
+12. Computer Science</p>
+
+<p>Artificial Intelligence<br />
+Information Theory</p>
+
+<p>13. Literature</p>
+
+<p>Poetry<br />
+Plath. Mad Girl&rsquo;s Love Song.<br />
+Rexroth. Vitamins and Roughage.</p>
+
+<p>Plays<br />
+Shakespeare. King Lear.<br />
+Shakespeare. Julius Caesar.</p>
+
+<p>Comedy<br />
+Kaufman. You can&rsquo;t take it with you.<br />
+Bloomingdale. I Should&rsquo;ve Seen it Coming when the Rabbit Died.</p>
+
+<p>Non-fiction<br />
+Hillenbrand. Unbroken. Story of WWII survival and redemption.</p>
+
+<p>Fiction<br />
+David Foster Wallace. Infinite Jest.<br />
+Quinn. Ishmael.<br />
+Marquez. 100 years of solitude<br />
+Caldwell. The Rule of Four.<br />
+Heller. Catch 22<br />
+Coelho. The Alchemist<br />
+Hesse. Siddhartha. He who has found his meaning, attained his goals. Buddhist.<br />
+Murukami. 1q84.<br />
+Dostoyevsky. The Brothers Karamazov<br />
+Wilde. The Picture of Dorian Gray<br />
+Saavedra. Don Quixote<br />
+Chbosky. The Perks of Being a Wallflower.<br />
+Kundera. The Unbearable Lightness of Being.<br />
+Dumas. The Three Musketteers. <br />
+Dumas. The Counte of Monte Cristo. Unabridged Version.<br />
+Hemmingway. The Sun Also Rises.<br />
+Patterson. Alex Cross Series.<br />
+Brooks. World War Z.<br />
+Adams. Hitchhiker&rsquo;s Guide to the Galaxy.<br />
+Rand. The Fountainhead.<br />
+Bradley. The Mists of Avalon. Legends from female perspectives.<br />
+Zusak. The Book Theif.</p>
+
+<p>Science Fiction<br />
+Collins. The Hunger Games.<br />
+Mitchell. Cloud Atlas. Post-apocalyptic story interweaving six stories.<br />
+Gibson. Neuromancer. Computer hacking.<br />
+Asimov. Foundation Series. Mathematic prediction of society.<br />
+Dick. Do Androids Dream of Electric Sheep?<br />
+Orson Scott Card. Ender&rsquo;s Game Series.<br />
+Anderson. Feed. Corporate power, consumerism, data and IT.<br />
+Yudkowsky. Harry Potter and the Methods of Rationality.<br />
+L&rsquo;Engle. A Wrinkle in Time.</p>
+
+<p>Fantasy<br />
+Martins. Game of Thrones Series<br />
+Rigney. Wheel of Time Series<br />
+White. The Once and Future King.<br />
+ <br />
+ <br />
+ <br />
+14. Personal Growth</p>
+
+<p>Athletics<br />
+Goldberg. Playing Out of Your Mind.<br />
+Goldberg. Sports Slump Busting.<br />
+Jackson. Sacred Hoops.<br />
+McDougall. Born to Run.<br />
+Wooden. A lifetime of Observations and Reflections.<br />
+Lemov. Practice Perfect.</p>
+
+<p>Personal Skills<br />
+Morgan. Warrior Mind. Martial arts strategy and philosophy<br />
+Cabane. The Charisma Myth.<br />
+Alder. How to Read a Book.<br />
+Greene. Mastery.<br />
+Loori. The Eight Gates of Zen. Zen Buddhism.<br />
+Pollan. The Omnivore&rsquo;s Dilemma.<br />
+Tolle. The Power of Now.<br />
+Loehr. The Power of Full Engagement<br />
+Waitzkin. The Art of Learning. Inner journey to optimal performance.<br />
+Ferris. 4 Hour Workweek.<br />
+Ferris. 4 Hour Body.<br />
+Didon. The Year of Magical Thinking.<br />
+Godin. Whatcha Gonna Do With That Duck? And Other Provocations.<br />
+Becker. Simplify. Minimalist living.<br />
+Jeffers. Feel the Fear and Do It Anyway<br />
+Ferrazzi. Never Eat Alone. Success through personal relationships.</p>
+
+<p>Relationships<br />
+Delis. The Passion Trap.<br />
+Comfort. The Joy of Sex.<br />
+Ryan. Sex at Dawn.<br />
+Greene. The Art of Seduction.<br />
+Navarro. What Every Body is Saying.</p>
+
+<p>15. Business</p>
+
+<p>Senge. The Fifth Discipline. Systems thinking, converting companies into learning organizations.<br />
+Ury. Positive No.<br />
+Chamine. Positive Intelligence. Achieving true potential for teams and individuals.<br />
+HBR&rsquo;s 10 must-reads On Strategy.<br />
+Siegel. Predictive Analytics. The power to predict.<br />
+Kaufman. The Personal MBA. Master the art of business.<br />
+Christensen. The Innovator&rsquo;s Dilemma.<br />
+Drucker. The Effective Executive. True expert on business management.</p>
+
+<p>Startups<br />
+Reis. The Lean Startup.<br />
+Wasserman. The Founder&rsquo;s Dilemmas.<br />
+Pink. To Sell is Human.<br />
+Bly. The Copywriter&rsquo;s Handbook. A guide to writing.</p>
+
+<p> <br />
+</p>
