48e24237838a9fef189ee6484019dcc718a1d3cc
jeremynixon <jnixon2@gmail.com>
2014-07-22 15:23:12 -0400

    landing page update

--- app/views/landing.php
+++ app/views/landing.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 date_default_timezone_set ('america/new_york');
 ?>
-
+	
 <!doctype html>
 
 <html>
@@ -11,16 +11,24 @@ date_default_timezone_set ('america/new_york');
 <head>
 	<title>Home</title>
 	<link rel=stylesheet type="text/css" href="style.css">
+	<style>
+	body
+{
+background-image:url('face.jpg');
+text-align: right;
+background-repeat: no-repeat	;
+}
+	</style>
 </head>
 
 <body>
 
 <h1>Welcome!</h1>
 
-<a href="ultimate-video.jeremynixon.me/open-club" target="_blank">Open Club</a><br><br>
-<a href="ultimate-video.jeremynixon.me/mixed-club" target="_blank">Mixed Club</a><br><br>
-<a href="ultimate-video.jeremynixon.me/women-club" target="_blank">Women's Club</a><br><br>
-<a href="ultimate-video.jeremynixon.me/open-college" target="_blank">College Open</a><br><br>	
+<a href="http://hello-world.jeremynixon.me/open-club" target="_blank">Open Club</a><br><br>
+<a href="hello-world.jeremynixon.me/mixed-club" target="_blank">Mixed Club</a><br><br>
+<a href="hello-world.jeremynixon.me/women-club" target="_blank">Women's Club</a><br><br>
+<a href="hello-world.jeremynixon.me/open-college" target="_blank">College Open</a><br><br>	
 
 </body>
 </html>
