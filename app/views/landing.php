b48b8eecbd4c150bf80e3a8df57b3ce9eb291436
jeremynixon <jnixon2@gmail.com>
2014-07-22 15:07:59 -0400

    change repo

---
+++ app/views/landing.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+date_default_timezone_set ('america/new_york');
+?>
+
+<!doctype html>
+
+<html>
+
+<head>
+	<title>Home</title>
+	<link rel=stylesheet type="text/css" href="style.css">
+</head>
+
+<body>
+
+<h1>Welcome!</h1>
+
+<a href="ultimate-video.jeremynixon.me/open-club" target="_blank">Open Club</a><br><br>
+<a href="ultimate-video.jeremynixon.me/mixed-club" target="_blank">Mixed Club</a><br><br>
+<a href="ultimate-video.jeremynixon.me/women-club" target="_blank">Women's Club</a><br><br>
+<a href="ultimate-video.jeremynixon.me/open-college" target="_blank">College Open</a><br><br>	
+
+</body>
+</html>
